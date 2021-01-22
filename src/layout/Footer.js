70cdf9b124f0adda63db24ae72a994998f4e6258
TolgaKara <tolgakara2.0@gmail.com>
2021-01-22 19:34:00 +0100

    Updated Footer & Header

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { FaLinkedin, FaGithubAlt } from "react-icons/fa";
 
 function Footer() {
-  return (
-    <footer className="mx-auto h-screen/10 flex justify-center items-center bg-teal-700 text-white">
-      <p>Made by Serhii &copy; 2020</p>
-    </footer>
-  );
-};
+	return (
+		<footer className='mx-auto mt-10 h-screen/10 flex justify-center items-center flex-col bg-green-800 text-white'>
+			<p>Created by Tolga Kara</p>
+
+			<a className='flex text-white' href='https://www.linkedin.com/in/tolgakara/' target='__blank'>
+				<FaLinkedin className='mt-1 mr-1' /> LinkedIn
+			</a>
+			<a className='flex text-white' href='https://github.com/TolgaKara/' target='__blank'>
+				<FaGithubAlt className='mt-1 mr-1' /> GitHub
+			</a>
+		</footer>
+	);
+}
 
 export default Footer;
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -3,40 +3,63 @@ import { NavLink, useHistory } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 function Header() {
-  const { currentUser, logout } = useAuth();
-  const history = useHistory()
+	const { login } = useAuth();
+	const history = useHistory();
 
-  async function handleLogout() {
-      try {
-      await logout()
-      history.push("/signin")
-      } catch {
-      console.log("Failed to log out")
-      }
-  }
+	function handleLogin() {
+		history.push("signin");
+	}
 
-  return (
-    <nav className="mx-auto p-4 h-screen/10 flex justify-between bg-teal-400 font-semibold">
-      <NavLink className="logo text-white" to="/">
-        Logo
-      </NavLink>
-      {currentUser ? 
-      <ul className="flex content-between">
-        <li className="px-1 text-white cursor-pointer hover:underline">
-          <NavLink to="/">Dashboard</NavLink>
-        </li>
-        <li className="px-1 text-white cursor-pointer hover:underline">
-          <NavLink to="/profile">Profile</NavLink>
-        </li>
-        <li className="px-1 text-white cursor-pointer hover:underline">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded text-sm" onClick={handleLogout}>Log Out</button>
-        </li>
-      </ul> 
-      : 
-      null
-       }
-    </nav>
-  );
-};
+	return (
+		<nav className='mx-auto shadow-md p-4 h-screen/8 flex justify-between bg-white font-semibold'>
+			<NavLink
+				className='logo px-1 mt-1 text-black border-2 p-2 pr-2 pl-2 rounded-lg border-green-500	'
+				to='/'
+			>
+				🌱 Seedly
+			</NavLink>
+			<ul className='flex content-between my-auto'>
+				<li className='px-1 mt-1 text-black-600 cursor-pointer hover:underline'>
+					<NavLink to='/'>About</NavLink>
+				</li>
+				<li className='px-1 mt-1 ml-5 text-black-600 cursor-pointer hover:underline'>
+					<NavLink to='/profile'>Features</NavLink>
+				</li>
+				<li className='px-1 mt-1 ml-5 text-black-600 cursor-pointer hover:underline'>
+					<NavLink to='/profile'>Testimonials</NavLink>
+				</li>
+				<li className='px-1 mt-1 ml-5 text-black-600 cursor-pointer hover:underline'>
+					<NavLink to='/profile'>Blog</NavLink>
+				</li>
+				<li className='px-1 ml-5 text-black-600 cursor-pointer hover:underline'>
+					<button
+						className='bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 rounded text-lg'
+						onClick={history.push("signin")}
+					>
+						Signin
+					</button>
+				</li>
+			</ul>
+			{/* {currentUser ? (
+				<ul className='flex content-between'>
+					<li className='px-1 text-white cursor-pointer hover:underline'>
+						<NavLink to='/'>Dashboard</NavLink>
+					</li>
+					<li className='px-1 text-white cursor-pointer hover:underline'>
+						<NavLink to='/profile'>Profile</NavLink>
+					</li>
+					<li className='px-1 text-white cursor-pointer hover:underline'>
+						<button
+							className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded text-sm'
+							onClick={handleLogin}
+						>
+							Log Out
+						</button>
+					</li>
+				</ul>
+			) : null} */}
+		</nav>
+	);
+}
 
 export default Header;
