f81e8a176cbd5c2bcc2cdad7b5d2b54a5c98b6cb
TolgaKara <tolgakara2.0@gmail.com>
2021-03-15 15:56:37 +0100

    Filled the cta section

--- src/components/Landingpage/Landingpage.jsx
+++ src/components/Landingpage/Landingpage.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { SectionSubHeading } from "../atom/SectionSubHeading";
+import heroImage from "../../images/heroImage.svg";
 
 export const Landingpage = () => {
-	return <div className='container mx-auto'>Hello</div>;
+	return (
+		<div>
+			<div className='container mx-auto my-2'>
+				<div className='cta'>
+					<SectionSubHeading text={"Our Mission"} />
+					<h1>Organize your Speciality Crop right now !</h1>
+					<img src={heroImage} alt='Engaging hero that showcases our Mission for Farmers' />
+				</div>
+			</div>
+		</div>
+	);
 };
---
+++ src/components/atom/SectionSubHeading.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SectionSubHeading = (props) => {
+	return <p className='uppercase text-opacity-95 font-mono font-bold	text-base'>{props.text}</p>;
+};
