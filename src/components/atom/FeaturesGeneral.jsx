ad2a9cb8782542796d2c285301d8ffd55d52394c
TolgaKara <tolgakara2.0@gmail.com>
2021-03-20 13:39:54 +0100

    Added Component for general features

---
+++ src/components/atom/FeaturesGeneral.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const FeaturesGeneral = () => {
+	return (
+		<div className='features-general'>
+			<div className='feature-performance'>
+				<img src='' alt='' />
+				<h2 className='feature-title'>Perfomance</h2>
+				<p className='feature-description'></p>
+			</div>
+			<div className='feature-security'></div>
+			<div className='feature-responsiveness'></div>
+		</div>
+	);
+};
