f5c0678f43f23f2a3eddc86398e2162b9f70c6c2
TolgaKara <tolgakara2.0@gmail.com>
2021-03-20 16:27:33 +0100

    Added Responsive Feature

--- src/components/atom/FeaturesGeneral.jsx
+++ src/components/atom/FeaturesGeneral.jsx
@@ -1,19 +1,34 @@
 import React from "react";
-import { BiRocket } from "react-icons/bi";
+import { BiRocket, BiShieldQuarter } from "react-icons/bi";
+import { GrStackOverflow } from "react-icons/gr";
 
 export const FeaturesGeneral = () => {
 	return (
-		<div className='features-general flex flex-wrap flex-row justify-between text-center'>
-			<div className='feature-performance w-4/12 bg-white p-4 shadow-lg'>
-				<BiRocket className='text-green-500 my-5 text-6xl block m-auto' />
+		<div className='features-general flex flex-row justify-between text-center space-x-3'>
+			<div className='group feature-performance w-4/12 bg-white p-4 shadow-md hover:bg-green-500 hover:text-white duration-300'>
+				<BiRocket className='text-green-500 my-5 text-6xl block m-auto ' />
 				<h2 className='feature-title font-semibold '>Perfomance</h2>
 				<p className='feature-description text-sm'>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, nihil? Molestiae
 					suscipit facilis optio eaque hic laboriosam officia ipsam.
 				</p>
 			</div>
-			<div className='feature-security'></div>
-			<div className='feature-responsiveness'></div>
+			<div className='feature-security w-4/12 bg-white p-4 shadow-md hover:bg-green-500 duration-300'>
+				<BiShieldQuarter className='text-green-500 my-5 text-6xl block m-auto' />
+				<h2 className='feature-title font-semibold '>Security</h2>
+				<p className='feature-description text-sm'>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, nihil? Molestiae
+					suscipit facilis optio eaque hic laboriosam officia ipsam.
+				</p>
+			</div>
+			<div className='feature-responsiveness w-4/12 bg-white p-4 shadow-md'>
+				<GrStackOverflow className='text-green-500 my-5 text-6xl block m-auto' />
+				<h2 className='feature-title font-semibold '>Responsive</h2>
+				<p className='feature-description text-sm'>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, nihil? Molestiae
+					suscipit facilis optio eaque hic laboriosam officia ipsam.
+				</p>
+			</div>
 		</div>
 	);
 };
