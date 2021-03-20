3a09175f5f0182dd8fc60fb54c5cb3beaef40981
TolgaKara <tolgakara2.0@gmail.com>
2021-03-20 14:15:27 +0100

    Updated Features

--- src/components/atom/Features.jsx
+++ src/components/atom/Features.jsx
@@ -4,7 +4,7 @@ import { SectionSubHeading } from "./SectionSubHeading";
 
 export const Features = () => {
 	return (
-		<div className='features mt-40'>
+		<div className='features mt-40 mb-20 flex-wrap'>
 			<SectionSubHeading text='More then just Farming' />
 			<h1 className='text-4xl font-bold text-green-500'>Features</h1>
 			<FeaturesGeneral />
