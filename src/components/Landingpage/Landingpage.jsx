1527118c7ff67b342bc7fe79105492c47d0c8c5b
TolgaKara <tolgakara2.0@gmail.com>
2021-03-22 13:23:53 +0100

    Updated the Landingpage by cleaning up and optimi

--- src/components/Landingpage/Landingpage.jsx
+++ src/components/Landingpage/Landingpage.jsx
@@ -1,29 +1,14 @@
 import React from "react";
 import { SectionSubHeading } from "../atom/SectionSubHeading";
-import heroImage from "../../images/heroImage.svg";
 import { Button } from "../atom/Button";
 import { Features } from "../atom/Features";
+import { Hero } from "../atom/Hero";
 
 export const Landingpage = () => {
 	return (
 		<div className=''>
 			<div className='container mx-auto mt-16 '>
-				<div className='cta flex justify-between items-center'>
-					<div className='w-3/12'>
-						<SectionSubHeading text={"Our Mission"} />
-						<h1 className='mb-4 leading-7 font-semibold text-green-700'>
-							Organize your Speciality Crop right now 🌻 !
-						</h1>
-						<p className='mb-8 text-lg leading-8'>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, recusandae tempora.
-							Nesciunt alias neque quaerat similique corrupti.
-						</p>
-						<Button type='action' text='Get Started' />
-					</div>
-					<div>
-						<img src={heroImage} alt='Engaging hero that showcases our Mission for Farmers' />
-					</div>
-				</div>
+				<Hero />
 				<Features />
 			</div>
 		</div>
