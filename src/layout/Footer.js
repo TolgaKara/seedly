82d62370de26291bb847cbb6432d6eba207d59fd
TolgaKara <tolgakara2.0@gmail.com>
2021-03-22 13:20:12 +0100

    Updated the design of footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,20 +3,20 @@ import { FaLinkedin, FaGithubAlt } from "react-icons/fa";
 
 function Footer() {
 	return (
-		<footer className='inset-0 h-32 bg-green-500 relative w-full mb-0'>
+		<footer className='inset-0 h-32 bg-green-500 relative w-full mb-0 flex justify-center'>
 			<div className='p-2 m-1 text-white'>
-				<p>© 2021 Tolga Kara All Rights Reserved</p>
+				<p className='bg-white text-green-600 p-4 rounded-full mb-4 font-bold shadow-lg'>
+					© 2021 Tolga Kara All Rights Reserved
+				</p>
 
-				<a
-					className='flex text-white'
-					href='https://www.linkedin.com/in/tolgakara/'
-					target='__blank'
-				>
-					<FaLinkedin className='mt-1 mr-1' /> LinkedIn
-				</a>
-				<a className='flex text-white' href='https://github.com/TolgaKara/' target='__blank'>
-					<FaGithubAlt className='mt-1 mr-1' /> GitHub
-				</a>
+				<div className='flex flex-row justify-between'>
+					<a className='flex' href='https://www.linkedin.com/in/tolgakara/' target='__blank'>
+						<FaLinkedin className='mt-1 mr-1' /> LinkedIn
+					</a>
+					<a className='flex' href='https://github.com/TolgaKara/' target='__blank'>
+						<FaGithubAlt className='mt-1 mr-1' /> GitHub
+					</a>
+				</div>
 			</div>
 		</footer>
 	);
