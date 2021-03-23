14067f6ce16437bcc7adbbc33999274689933bb8
TolgaKara <tolgakara2.0@gmail.com>
2021-03-23 09:06:21 +0100

    Refactored The FeatureCore to have single componen

--- src/components/atom/FeaturesCore.jsx
+++ src/components/atom/FeaturesCore.jsx
@@ -1,23 +1,10 @@
 import React from "react";
-import featureMonitoring from "../../images/featureMonitoring.svg";
+import { FeatureCoreManaging } from "./FeatureCoreManaging";
 
 export const FeaturesCore = () => {
 	return (
-		<div>
-			<div className='flex justify-between items-center'>
-				<div className='w-3/12'>
-					<h2 className='mb-4 leading-7 font-semibold text-green-500'>
-						Manage your Crops in <span className='font-bold'>ONE</span> Place
-					</h2>
-					<p className='mb-8 text-lg leading-8'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, recusandae tempora.
-						Nesciunt alias neque quaerat similique corrupti.
-					</p>
-				</div>
-				<div>
-					<img src={featureMonitoring} alt='Engaging hero that showcases our Mission for Farmers' />
-				</div>
-			</div>
+		<div className='mt-20 space-y-4'>
+			<FeatureCoreManaging />
 		</div>
 	);
 };
