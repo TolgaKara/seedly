759a59225122c0d6a7eb34b582fd6cb2371b834b
TolgaKara <tolgakara2.0@gmail.com>
2021-03-28 06:50:28 +0200

    Updated Table with the Price starter

--- src/components/atom/PricingTable.jsx
+++ src/components/atom/PricingTable.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import PricingStarter from "./PricingStarter";
+import { SectionSubHeading } from "./SectionSubHeading";
 
 export const PricingTable = () => {
-	return <div></div>;
+	return (
+		<div>
+			<SectionSubHeading text='Find the right Price for the right Farm' />
+			<h1 className='text-4xl font-bold text-green-500 mb-4'>Pricing</h1>
+
+			<div class='flex flex-row flex-nowrap'>
+				<PricingStarter />
+			</div>
+		</div>
+	);
 };
