948ce4977aa4ae338083e63e56fe6dfddeac9329
TolgaKara <tolgakara2.0@gmail.com>
2021-03-28 06:50:14 +0200

    Created component for price starter

---
+++ src/components/atom/PricingStarter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button } from "./Button";
+
+const PricingStarter = () => {
+	return (
+		<div class='w-4/12 text-center shadow-md p-5'>
+			<p class='text-6xl mb-5'>👨‍🌾</p>
+			<p class='text-3xl font-semibold'>FREE</p>
+			<h3 class='text-green-500 font-bold text-lg uppercase border-b-4 border-green-500'>
+				Starter
+			</h3>
+			<ul class='list-none space-y-4 mt-4'>
+				<li class='font-medium text-base'>Lorem ipsum dolor sit amet</li>
+				<li class='font-medium text-base'>Lorem ipsum dolor sit amet</li>
+				<li class='font-medium text-base'>Lorem ipsum dolor sit amet</li>
+				<li class='font-medium text-base'>Lorem ipsum dolor sit amet</li>
+			</ul>
+			<Button type='action' text='Purchase' />
+		</div>
+	);
+};
+
+export default PricingStarter;
