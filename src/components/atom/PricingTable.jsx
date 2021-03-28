64f2939a5063f26aed2306c804eff2d66a189bf5
TolgaKara <tolgakara2.0@gmail.com>
2021-03-28 06:59:25 +0200

    Added enterprise to the table

--- src/components/atom/PricingTable.jsx
+++ src/components/atom/PricingTable.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { PricingBusiness } from "./PricingBusiness";
+import { PricingEnterprise } from "./PricingEnterprise";
 import PricingStarter from "./PricingStarter";
 import { SectionSubHeading } from "./SectionSubHeading";
 
@@ -12,6 +13,7 @@ export const PricingTable = () => {
 			<div class='flex flex-row flex-nowrap space-x-6'>
 				<PricingStarter />
 				<PricingBusiness />
+				<PricingEnterprise />
 			</div>
 		</div>
 	);
